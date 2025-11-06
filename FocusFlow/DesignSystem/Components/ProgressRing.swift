e1bc371491f8a1b3689aafe6fdc832aaac44f0e5
Bruno Neto <2022131815@my.ipleiria.pt>
2025-11-06 17:14:58 +0000

    Restructure UI folders and add ProgressRing component
    Moved .gitkeep files to new DesignSystem and Features directories to better organize components, screens, and themes. Added initial ProgressRing SwiftUI component. Added Xcode scheme management files.

---
+++ FocusFlow/DesignSystem/Components/ProgressRing.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  		
+//
+//  Created by formando on 06/11/2025.
+//
+import SwiftUI
+import Foundation
+
+public struct ProgressRing:View {
+    //Progresso 0-1
+    public var progress :Double
+    public var lineWidth 
+}
